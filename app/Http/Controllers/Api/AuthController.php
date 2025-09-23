452b29cbada99ed20bd037d841ab5003b1e131d3
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2025-09-23 17:37:19 +0600

    feat: create tickets, and user resource with conditional incllude in controller with proper json structure as response

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginUserRequest;
 use App\Models\User;
 use App\Traits\ApiResponses;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthController extends Controller
 {
@@ -25,13 +27,24 @@ class AuthController extends Controller
         return $this->ok(
             'Authenticated',
             [
-                'token' => $user->createToken('API token for ' . $user->email)->plainTextToken
+                'token' => $user->createToken(
+                    "Api token for {$user->email}",
+                    ['*'],
+                    now()->addDays(2)
+                )->plainTextToken
             ]
         );
     }
 
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->ok('Logout successful');
+    }
+
     public function register()
     {
-        return $this->ok('Register endpoint not implemented', []);
+        return $this->ok('Register endpoint not implemented');
     }
 }
